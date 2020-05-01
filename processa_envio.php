2c8d41f9b60a9289f699478c6a83a1979ab98cab
CleuJunior <cleu.junior@gmail.com>
2020-04-30 22:08:01 -0300

    Criando a Clas Mensagem e criando atributos e metodos

--- processa_envio.php
+++ processa_envio.php
@@ -1,3 +1,64 @@
 <?php
 
-print_r($_POST['assunto']);
+
+class Mensagem{
+    private $para = null;
+    private $assunto = null;
+    private $mensagem = null;
+
+    public function getPara()
+    {
+        return $this->para;
+    }
+
+    public function setPara($para)
+    {
+        $this->para = $para;
+    }
+
+
+    public function getAssunto()
+    {
+        return $this->assunto;
+    }
+
+
+    public function setAssunto($assunto)
+    {
+        $this->assunto = $assunto;
+    }
+
+    public function getMensagem()
+    {
+        return $this->mensagem;
+    }
+
+    public function setMensagem($mensagem)
+    {
+        $this->mensagem = $mensagem;
+    }
+
+    public function mensagemValida()
+    {
+        if(empty($this->para) || empty($this->assunto) || empty($this->mensagem)){
+            return false;
+        }
+        return true;
+    }
+
+}
+
+$mensagem = new Mensagem();
+
+$mensagem->setPara($_POST['para']);
+$mensagem->setAssunto($_POST['assunto']);
+$mensagem->setMensagem($_POST['mensagem']);
+
+//print_r($mensagem);
+
+if ($mensagem->mensagemValida()){
+    echo "Mensagem valida";
+
+}else{
+    echo "Mensagem não valida";
+}
