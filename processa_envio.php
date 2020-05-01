3ee96a1fc809b21b4cd3905f327bb67fd28fa26a
CleuJunior <cleu.junior@gmail.com>
2020-05-01 00:31:25 -0300

    Biblioteca adicionada, PHPMailer

--- processa_envio.php
+++ processa_envio.php
@@ -1,7 +1,16 @@
 <?php
 
+require "./bibliotecas/PHPMailer/Exception.php";
+require "./bibliotecas/PHPMailer/OAuth.php";
+require "./bibliotecas/PHPMailer/PHPMailer.php";
+require "./bibliotecas/PHPMailer/POP3.php";
+require "./bibliotecas/PHPMailer/SMTP.php";
 
-class Mensagem{
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+class Mensagem
+{
     private $para = null;
     private $assunto = null;
     private $mensagem = null;
@@ -40,7 +49,7 @@ class Mensagem{
 
     public function mensagemValida()
     {
-        if(empty($this->para) || empty($this->assunto) || empty($this->mensagem)){
+        if (empty($this->para) || empty($this->assunto) || empty($this->mensagem)) {
             return false;
         }
         return true;
@@ -54,11 +63,45 @@ $mensagem->setPara($_POST['para']);
 $mensagem->setAssunto($_POST['assunto']);
 $mensagem->setMensagem($_POST['mensagem']);
 
-//print_r($mensagem);
 
-if ($mensagem->mensagemValida()){
-    echo "Mensagem valida";
+if (!$mensagem->mensagemValida()) {
+    echo "Mensagem não é valida";
+    die();
+}
 
-}else{
-    echo "Mensagem não valida";
+$mail = new PHPMailer(true);
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'user@example.com';                 // SMTP username
+    $mail->Password = 'secret';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('from@example.com', 'Mailer');
+    $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
+    $mail->addAddress('ellen@example.com');               // Name is optional
+    $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Não foi possível enviar este email. <br>';
+    echo 'Erro: ' . $mail->ErrorInfo;
 }
